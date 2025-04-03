e12b2cb25f3f140e6b5b524181f9b45b43faeb56
Tala Dweik <227007@ppu.edu.ps>
2025-04-03 22:06:31 +0200

    first commit

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,33 +1,69 @@
+import orderModel from '../models/orderModel.js';
 
 
-// Placing Orders Using COD Method 
+// Placing Orders Using Cash On Delivery Method 
 const placeOrder = async (req, res) => {
+    try {
+        const { userId, items, amount, address } = req.body;
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: "COD",
+            payment: false,
+            date: Date.now()
+        }
+        const newOrder = new orderModel(orderData)
 
-}
+        await newOrder.save()
+        await userModel.findByIdAndUpdate(userId, { carrtData: {} })
+        res.json({ success: true, message: "Order Placed" })
 
-// Placing Orders Using Stripe Method 
-const placeOrderStripe = async (req, res) => {
-
-}
-
-// Placing Orders Using Rezorpay Method 
-const placeOrderRezorpay = async (req, res) => {
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
 // All Orders Data For Admin Panel 
 const allOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({ userId })
+        res.json({ success: true, orders })
 
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // User Orders Data For Frontend
 const userOrders = async (req, res) => {
+    try {
+        const { userId } = req.body
+        const orders = await orderModel.find({ userId })
+        res.json({ success: true, orders })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
 // Update Order Status From Admin Panel 
 const updateStatus = async (req, res) => {
+    try {
+        const { orderId, status } = req.body
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({ success: true, message: 'Status Updated' })
 
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
 
-export {placeOrder, placeOrderStripe, placeOrderRezorpay, allOrders, userOrders, updateStatus}
+export { placeOrder,allOrders, userOrders, updateStatus }
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {placeOrder, placeOrderStripe, placeOrderRezorpay, allOrders, userOrders, updateStatus} from '../controllers/orderController.js'
+import {placeOrder, allOrders, userOrders, updateStatus} from '../controllers/orderController.js'
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js';
 
@@ -11,8 +11,6 @@ orderRouter.post('./status',adminAuth,updateStatus)
 
 // Payment Features 
 orderRouter.post('./place',authUser,placeOrder)
-orderRouter.post('./stripe',authUser,placeOrderStripe)
-orderRouter.post('./razorpay',authUser,placeOrderRezorpay)
 
 // User Feature
 orderRouter.post('./userorders',authUser,userOrders)
