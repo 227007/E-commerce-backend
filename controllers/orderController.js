20da1aca8364e6e59b9166cc6cdbe593dbcba1e0
Tala Dweik <227007@ppu.edu.ps>
2025-04-03 20:46:50 +0200

    first commit

---
+++ controllers/orderController.js
@@ -0,0 +1,33 @@
+
+
+// Placing Orders Using COD Method 
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing Orders Using Stripe Method 
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// Placing Orders Using Rezorpay Method 
+const placeOrderRezorpay = async (req, res) => {
+
+}
+
+// All Orders Data For Admin Panel 
+const allOrders = async (req, res) => {
+
+}
+
+// User Orders Data For Frontend
+const userOrders = async (req, res) => {
+
+}
+
+// Update Order Status From Admin Panel 
+const updateStatus = async (req, res) => {
+
+}
+
+export {placeOrder, placeOrderStripe, placeOrderRezorpay, allOrders, userOrders, updateStatus}
